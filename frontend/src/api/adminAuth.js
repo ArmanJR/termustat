276311257bb6606f2d7d69af15c95e9911bcdfc6
Erfan Azarmi <erfanazarmi1@gmail.com>
2025-04-14 20:32:47 +0330

    create adminAuth.js and implement adminLogin function

---
+++ frontend/src/api/adminAuth.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+export async function adminLogin(credentials) {
+  try {
+    const response = await axios.post(
+      "http://localhost:8080/api/v1/auth/login",
+      credentials,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    // On successful login, store the token in local storage
+    const token = response.data.token;
+    localStorage.setItem("token", token);
+
+    console.log("Login successful, token saved!");
+    alert("Login successful, token saved!");
+  } catch (error) {
+    console.error("Login failed:", error.response?.data || error.message);
+    alert("Login failed:", error.response?.data || error.message);
+  }
+};
