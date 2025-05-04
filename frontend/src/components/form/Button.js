8077b100e1b15fbf83aa728a2a421185461c2e00
Erfan Azarmi <erfanazarmi1@gmail.com>
2025-05-05 02:05:49 +0330

    refactor: add style and className props to Button component

--- frontend/src/components/form/Button.js
+++ frontend/src/components/form/Button.js
@@ -1,11 +1,12 @@
 import styles from './Button.module.css';
 
-const Button = ({type = "button", value, onClick = () => {}}) => {
+const Button = ({type = "button", value, onClick = () => {}, style, className}) => {
   return (
     <button
-      className={styles.button}
+      className={`${styles.button} ${className || ''}`}
       type={type}
       onClick={onClick}
+      style={style}
     >
       {value}
     </button>
