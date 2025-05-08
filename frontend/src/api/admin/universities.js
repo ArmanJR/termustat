947fc660bf3477284c40dd51eeba80134b35a077
Erfan Azarmi <erfanazarmi1@gmail.com>
2025-05-08 19:34:33 +0330

    Implement university CRUD functions to interact with API

---
+++ frontend/src/api/admin/universities.js
@@ -0,0 +1,31 @@
+import axiosInstance from "../axiosInstance";
+
+export const getUniversities = async () => {
+  const response = await axiosInstance.get(
+    "/admin/universities"
+  );
+  return response.data;
+};
+
+export const addUniversity = async (universityData) => {
+  const response = await axiosInstance.post(
+    "/admin/universities",
+    universityData
+  );
+  return response.data;
+};
+
+export const deleteUniversity = async (id) => {
+  const response = await axiosInstance.delete(
+    `/admin/universities/${id}`
+  );
+  return response;
+};
+
+export const editUniversity = async (id, universityData) => {
+  const response = await axiosInstance.put(
+    `/admin/universities/${id}`,
+    universityData
+  );
+  return response.data;
+};
